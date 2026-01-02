1bad68cb18bc033e241b6be0223eb732b50e9771
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2026-01-02 20:47:10 +0600

    update 5 files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/registerUser.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,9 +8,9 @@ export class AuthController {
     this.authService = authService;
   }
   @Post('register')
-  register() {
+  register(@Body() registerUserDto: RegisterDto) {
     //logic for user register
-    const result = this.authService.registerUser();
+    const result = this.authService.registerUser(registerUserDto);
     return result;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './dto/registerUser.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
-  registerUser() {
-    return this.userService.createUser();
-    return { message: 'User Registered Successfully!' };
+  async registerUser(registerUserDto: RegisterDto) {
+    console.log(registerUserDto);
+    const hash = await bcrypt.hash(registerUserDto.password, 10);
+    return this.userService.createUser({ ...registerUserDto, password: hash });
   }
 }
