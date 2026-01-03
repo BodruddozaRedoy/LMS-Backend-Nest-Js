57694c14a2334fe117127a513a993ce89335f5ca
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2026-01-03 14:54:10 +0600

    update auth.guard.ts, auth.module.ts, roles.guard.ts and course.module.ts

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -3,10 +3,12 @@ import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Course, CourseSchema } from './schemas/course.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Course.name, schema: CourseSchema }]),
+    AuthModule,
   ],
   controllers: [CourseController],
   providers: [CourseService],
